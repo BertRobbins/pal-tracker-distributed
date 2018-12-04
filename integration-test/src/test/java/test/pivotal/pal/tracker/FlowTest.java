7372e2a54931b005b21999ccbe24e8641d6c259c
bert.robbins@dell.com <bert.robbins@dell.com>
2018-12-04 15:45:13 -0700

    defect fix

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -154,6 +154,8 @@ public class FlowTest {
 
         response = httpClient.get(timesheetsServerUrl("/time-entries?projectId" + createdProjectId));
         assertThat(response.body).isNotNull().isNotEmpty();
+
+        
     }
 }
 
